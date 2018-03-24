2468b05cbd82c929d4f4cf02a19fe26df7dc68d3
Aleksandar Babic <alekski64@icloud.com>
2018-03-24 21:01:19 +0100

    Initial commit

---
+++ Airslamit/Gcr/Block/Success.php
@@ -0,0 +1,29 @@
+<?php
+namespace Airslamit\Gcr\Block;
+class Success extends \Magento\Framework\View\Element\Template
+{
+    protected $_checkoutSession;
+ 
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession) {
+
+        parent::__construct($context);
+        $this->_checkoutSession = $checkoutSession;
+    }
+
+    public function getSomething()
+    {
+        return 'returned something from custom block.';
+    }
+
+    public function getOrder() {
+        return $this->_checkoutSession->getLastRealOrder();
+    }
+
+    public function getOrderId() {
+        $order = $this->_checkoutSession->getLastRealOrder();
+        return $order->getEntityId();
+    }
+
+}
